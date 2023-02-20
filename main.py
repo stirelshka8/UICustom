85d036da02271c17a7a288e1b12cf4554a7d5d28
stirelshka8 <stirelshka8@gmail.com>
2023-02-20 21:18:14 +0300

    UI

--- main.py
+++ main.py
@@ -4,28 +4,30 @@ customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark
 customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
-class App(customtkinter.CTk):
+class Main_window(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("KREAM data collection program v0.2a")
-        self.geometry(f"{1100}x{580}")
+        self.geometry(f"{800}x{580}")
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Парсер KREAM", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Парсер KREAM",
+                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_setting = customtkinter.CTkButton(self.sidebar_frame, text="Настройки", command=self.sidebar_button_event)
-        self.sidebar_button_setting.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Информация", command=self.sidebar_button_event)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Информация",
+                                                        command=self.info_dialog)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.string_input_button = customtkinter.CTkButton(self.sidebar_frame, text="Настройки",
+                                                           command=self.ruuuuu)
+        self.string_input_button.grid(row=2, column=0, padx=20, pady=(70, 10))
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Тема:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
@@ -39,6 +41,13 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
+    def ruuuuu(self):
+        print("FACK")
+
+    def info_dialog(self):
+        app_second = Second_window()
+        app_second.mainloop()
+
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
@@ -50,7 +59,18 @@ class App(customtkinter.CTk):
         print("sidebar_button click")
 
 
+class Second_window(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Информация")
+        self.geometry(f"{400}x{250}")
+        self.textbox = customtkinter.CTkTextbox(self, width=360)
+        self.textbox.grid(row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.textbox.insert("0.0", "Парсер данных с сайта www.cream.co.kr\n\n" + "Парам пам пам\n\n")
+        self.textbox.configure(state="disabled")
+
 
 if __name__ == "__main__":
-    app = App()
+    app = Main_window()
     app.mainloop()
